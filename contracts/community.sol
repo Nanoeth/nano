59056fe5975ea153184b60f1dc8c927cc5c01a56
yann300 <yann.levreau@gmail.com>
2017-04-05 18:33:57 +0200

    remove wallet.sol && refactor ui

--- contracts/community.sol
+++ contracts/community.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.4.0;
-import "wallet.sol";
 import "config.sol";
 
 contract community {
     config private conf;
-    mapping (address => wallet) public wallets;
+    mapping (address => wallet) public value;
     mapping (bytes32 => address) public walletsNS;
-    bytes32[] public walletNames;
     event report(string _arg);
+    
+    struct wallet {
+        address owner;
+        uint value;
+        uint lastRefillBlockNumber;
+        uint originBlockNumber;
+    }
     
     function community () {
         conf = new config();
@@ -18,58 +23,70 @@ contract community {
     }
     
     function createWallet (bytes32 _name) {
-        report('wallet creation ...');
-        wallets[msg.sender] = new wallet(msg.sender, this);
-        report('wallet created');
+        wallet memory newWallet;
+        newWallet.owner = msg.sender;
+        newWallet.value = 0;
+        newWallet.lastRefillBlockNumber = 0;
+        newWallet.originBlockNumber = 0;
+        value[msg.sender] = newWallet;
         walletsNS[_name] = msg.sender;
-        walletNames.length = walletNames.length + 1;
-        walletNames[walletNames.length - 1] = _name;
-        report('end createWallet');
+        walletsNS[_name] = msg.sender;
+        report('createWallet ok');
     }
     
-    function walletByName (bytes32 _name) constant returns (wallet) {
-        return wallets[walletsNS[_name]];
+    function walletByName (bytes32 _name) constant internal returns (wallet) {
+        return value[walletsNS[_name]];
     }
     
-    function walletByAddress (address _address) constant returns (wallet) {
-        return wallets[_address];
+    function walletByAddress (address _address) constant internal returns (wallet) {
+        return value[_address];
     }
     
     function requestRefill () {
-        wallet w = walletByAddress(msg.sender);
-        if (int(block.number) - w.getLastRefillBlockNumber() > conf.getTimeToZero()) {
-           w.setValue(conf.getRefillBalance());
-           w.setOriginBlock(int(block.number));
+        if (block.number - value[msg.sender].lastRefillBlockNumber > conf.getTimeToZero()) {
+            value[msg.sender].value = conf.getRefillBalance();
+            value[msg.sender].originBlockNumber = block.number;
+            return report("requestRefill ok");
         }
     }
     
-    function send (bytes32 _from, bytes32 _to, int _value) {
+    function currentValueOf(address _owner) constant returns (uint) {
+        wallet memory w = walletByAddress(_owner);
+        return conf.currentValue(w.value, w.originBlockNumber, block.number);
+    }
+    
+    function originBlockNumebrOf(address _owner) constant returns (uint) {
+        wallet memory w = walletByAddress(_owner);
+        return w.originBlockNumber;
+    }
+    
+    function send (bytes32 _from, bytes32 _to, uint _value) {
         if (_value <= 0) {
             return report("no value or negative");
         }
-        wallet to = walletByName(_to);
-        if (address(to) == address(0)) {
+        wallet memory to = walletByName(_to);
+        if (to.owner == address(0)) {
             return report("sender not found");
         }
-        wallet from = walletByName(_from);
-        if (address(from) == address(0)) {
+        wallet memory from = walletByName(_from);
+        if (from.owner == address(0)) {
             return report("receiver not found");
         }
-        int fromValue = from.getValue();
-        int toValue = to.getValue();
+        uint fromValue = conf.currentValue(from.value, from.originBlockNumber, block.number);
+        uint toValue = conf.currentValue(to.value, to.originBlockNumber, block.number);
         if (_value > fromValue) {
             return report("not enough balance");
         }
         // update sender
-        from.setValue(fromValue - _value);
+        value[from.owner].value = fromValue - _value;
         // update receiver
-        to.setValue(toValue + _value);
-        int origin = to.getOriginBlockNumber();
-        if (int(block.number) - origin >= conf.getTimeToZero()) {
-            to.setOriginBlock(int(block.number)); 
+        value[to.owner].value = toValue + _value;
+        uint origin = to.originBlockNumber;
+        if (block.number - origin >= conf.getTimeToZero()) {
+            value[to.owner].originBlockNumber = block.number;
         } else {
-            int blockNumberIncr = conf.blockNumberOriginIncr(_value);
-            to.setOriginBlock(origin + blockNumberIncr);    
+            uint blockNumberIncr = conf.blockNumberOriginIncr(_value);
+            value[to.owner].originBlockNumber = origin + blockNumberIncr;    
         }
         return report("send ok");
     }
--- contracts/config.sol
+++ contracts/config.sol
@@ -1,24 +1,24 @@
 
 pragma solidity ^0.4.0;
 contract config {
-    int private timeToZero = 300; // 1 day <=> 5760 blocks
-    int private burningTime = 300;
-    int private refillBalance = 500;
+    uint private timeToZero = 300; // 1 day <=> 5760 blocks
+    uint private burningTime = 300;
+    uint private refillBalance = 500;
     
-    function currentValue (int value, int originBlockNumber, int blockNumber) returns (int) {
+    function currentValue (uint value, uint originBlockNumber, uint blockNumber) returns (uint) {
         if (blockNumber - originBlockNumber > timeToZero) {
             return 0;
         } else if (blockNumber - originBlockNumber <= timeToZero - burningTime) {
             return value;
         } else {
-            int coeff = (blockNumber - originBlockNumber - timeToZero + burningTime) * 1000;
+            uint coeff = (blockNumber - originBlockNumber - timeToZero + burningTime) * 1000;
             coeff = coeff / burningTime;
             coeff = 1000 - coeff;
             return (coeff * value) / 1000;
         }
     }
     
-    function blockNumberOriginIncr (int _value) returns (int) {
+    function blockNumberOriginIncr (uint _value) returns (uint) {
         if (_value >= refillBalance) {
             return timeToZero; //the transfered value is more than the refill balance, we give time to zero.
         } else {
@@ -26,19 +26,19 @@ contract config {
         }
     }
     
-    function getTimeToZero() returns (int) {
+    function getTimeToZero() returns (uint) {
         return timeToZero;
     }
     
-    function setTimeToZero(int _time) {
+    function setTimeToZero(uint _time) {
         timeToZero = _time;
     }
     
-     function getRefillBalance() returns (int) {
+     function getRefillBalance() returns (uint) {
         return refillBalance;
     }
     
-    function setRefillBalance(int _balance) {
+    function setRefillBalance(uint _balance) {
         refillBalance = _balance;
     }
 }
